19a14a830416e33a834d210082b5095e6ed46a1a
ndeakin <nickrdeakin@gmail.com>
2014-02-13 23:41:05 -0500

    Added Sprites namespace.
    Added the Sprites namespace to contain vario
    us sprite data, such as sprite locations on sheets. The namespace was started off with the
    locations of a left facing Skier sprite and a right facing Skier Sprite.

---
+++ src/skier_sprites.hpp
@@ -0,0 +1,11 @@
+#ifndef _SKIER_SPRITES_HPP
+#define _SKIER_SPRITES_HPP
+
+namespace Sprites {
+  // Skier class sprite locations
+  
+  sf::IntRect SKIER_45( 60, 65, 55, 50 );
+  sf::IntRect SKIER_n45( 18, 120, 40, 42 );
+}
+
+#endif // !_SKIER_SPRITES_HPP
