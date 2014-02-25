e71e073de306a3b8e5fa94d8f81e5ace960dd3b2
ndeakin <nickrdeakin@gmail.com>
2014-02-25 02:51:48 -0500

    Added additional comments and initially forgot the ctor in the header for the Moving_game_object class.

--- src/moving_game_object.hpp
+++ src/moving_game_object.hpp
@@ -5,7 +5,13 @@
 
 class Moving_game_object : public Visible_game_object {
   public:
-    void Update( sf::Time elapsed_time );
+    Moving_game_object()
+    {}
+
+    // Called with the elapsed time since the previous call.
+    virtual void Update( sf::Time elapsed_time );
+
+    // Move x and y away from current position.
     void Move( float x, float y );
 }
 
