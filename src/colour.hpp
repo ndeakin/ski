8cec42074a35895b78422cc980aaca71bdb85b47
ndeakin <nickrdeakin@gmail.com>
2014-03-04 23:37:44 -0500

    Added the Colour namespace.

---
+++ src/colour.hpp
@@ -0,0 +1,13 @@
+#ifndef _COLOUR_HPP_INCLUDED
+#define _COLOUR_HPP_INCLUDED
+
+namespace Colour {
+
+enum Colour {
+    RED,
+    BLUE
+};
+
+} // namespace Colour
+
+#endif // !_COLOUR_HPP_INCLUDED
